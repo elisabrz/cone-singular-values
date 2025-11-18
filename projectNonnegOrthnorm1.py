c1bdebc2d23b93d8e98f1c1b130f1c2574be15d7
elisabrz <elisa.bourez@gmail.com>
2025-11-18 11:50:29 +0100

    Fonction commentée pour orthon positif

--- projectNonnegOrthnorm1.py
+++ projectNonnegOrthnorm1.py
@@ -1,6 +1,16 @@
 """
 Project on norm1 nonnegative orthant
+
+On veut trouver le vecteur u qui maximise le produit scalaire avec Av
+u ≥ 0 (toutes les composantes sont positives ou nulles) et ||u|| = 1
+
 max_{u>=0,||u||=1} u^T Av
+
+Pour que ce produit soit maximum, on veut que u[i] soit grand quand Av[i] est négatif
+Donc on prend -Av (pour inverser les signes) et on garde uniquement les valeurs positives
+Si Av n'a que des valeurs positives, alors u serait complètement nul.
+On cherche alors l'indice où Av est le moins positif (ou le plus négatif)
+On met u[b] = 1 sur cet indice seulement
 """
 
 import numpy as np
